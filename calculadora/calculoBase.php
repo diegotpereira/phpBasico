1c3fc0c289a605f4e595330a0fb0cba1928acbb0
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-17 12:04:55 -0300

    criação do arquivo calculoBase.php e calcBase.php demonstração em arquivos separados pq não acha variavel key.

---
+++ calculadora/calculoBase.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Curso PHP Progressivo</title>
+ </head>
+ <body>
+ <form action='calcBase.php' method="get">
+  Nota 1: <input type="text" name="num1"><br>
+  Nota 2: <input type="text" name="num2"><br>
+  Nota 3: <input type="text" name="num3"><br>
+  <input type="submit">
+ </form>
+ </body>
+</html>
